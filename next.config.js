7819a1b1f17ed86d6f0cabafefdcefccab8fd494
cavaldos <khanhnguyen297kn@gmail.com>
2023-09-28 21:53:37 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+  server: {
+    // Chạy dưới địa chỉ IP host "0.0.0.0"
+    host: "0.0.0.0",
+  },
+};
 
 module.exports = nextConfig
